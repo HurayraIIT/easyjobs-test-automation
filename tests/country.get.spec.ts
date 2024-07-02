5fe458e7aba1cc36046f63e06c06fe8d6309e284
Abu Hurayra <hurayraiit@gmail.com>
2024-07-02 22:52:14 +0600

    added test cases

---
+++ tests/country.get.spec.ts
@@ -0,0 +1,59 @@
+// GET: /api/v2/country
+
+import { test, expect } from '@playwright/test';
+import { createAuthHeaders } from '@datafactory/auth';
+import { createAssertions } from "@helpers/createAssertions";
+
+test.describe("/api/v2/country GET requests @company", async () => {
+    const companyEmail = `${process.env.COMPANY_EMAIL}`;
+    const companyPassword = `${process.env.COMPANY_PASSWORD}`;
+
+    test("GET with valid credentials @happy", async ({ request }) => {
+        let authHeaders = await createAuthHeaders(companyEmail, companyPassword);
+        const response = await request.get(`/api/v2/country`, {
+            headers: authHeaders
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body[0].id).toBe(1);
+        expect(body[0].sort_name).toBe("AF");
+        expect(body[0].name).toBe("Afghanistan");
+        expect(body[0].phone_code).toBe("93");
+        expect(body[0].nationality).toBe("Afghan");
+
+        expect(body[246].id).toBe(246);
+        expect(body[246].sort_name).toBe("ZW");
+        expect(body[246].name).toBe("Zimbabwe");
+        expect(body[246].phone_code).toBe("263");
+        expect(body[246].nationality).toBe("Zimbabwean");
+    });
+
+    test("GET without valid credentials @happy", async ({ request }) => {
+        const response = await request.get(`/api/v2/country`, {
+            headers: {
+                "ACCEPT": "application/json",
+            }
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body[0].id).toBe(1);
+        expect(body[0].sort_name).toBe("AF");
+        expect(body[0].name).toBe("Afghanistan");
+        expect(body[0].phone_code).toBe("93");
+        expect(body[0].nationality).toBe("Afghan");
+
+        expect(body[246].id).toBe(246);
+        expect(body[246].sort_name).toBe("ZW");
+        expect(body[246].name).toBe("Zimbabwe");
+        expect(body[246].phone_code).toBe("263");
+        expect(body[246].nationality).toBe("Zimbabwean");
+    });
+});
---
+++ tests/job/job-information-meta-data.get.spec.ts
@@ -0,0 +1,121 @@
+// GET: /api/v2/job/information-meta-data
+
+import { test, expect } from '@playwright/test';
+import { createAuthHeaders } from '@datafactory/auth';
+import { createAssertions } from "@helpers/createAssertions";
+
+test.describe("/api/v2/job/information-meta-data GET requests @company", async () => {
+    const companyEmail = `${process.env.COMPANY_EMAIL}`;
+    const companyPassword = `${process.env.COMPANY_PASSWORD}`;
+
+    test("GET with valid credentials @happy", async ({ request }) => {
+        let authHeaders = await createAuthHeaders(companyEmail, companyPassword);
+        const response = await request.get(`/api/v2/job/information-meta-data`, {
+            headers: authHeaders
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body.employment_type[0].id).toBe(1);
+        expect(body.employment_type[0].name).toBe("Permanent");
+        expect(body.employment_type[1].id).toBe(2);
+        expect(body.employment_type[1].name).toBe("Contractual");
+        expect(body.employment_type[2].id).toBe(3);
+        expect(body.employment_type[2].name).toBe("Full Time");
+        expect(body.employment_type[3].id).toBe(4);
+        expect(body.employment_type[3].name).toBe("Part Time");
+        expect(body.employment_type[4].id).toBe(5);
+        expect(body.employment_type[4].name).toBe("Freelance");
+        expect(body.employment_type[5].id).toBe(6);
+        expect(body.employment_type[5].name).toBe("Internship");
+        expect(body.employment_type[6].id).toBe(7);
+        expect(body.employment_type[6].name).toBe("College Program");
+        expect(body.employment_type[7].id).toBe(99);
+        expect(body.employment_type[7].name).toBe("Other");
+        expect(body.salary_type[0].id).toBe(1);
+        expect(body.salary_type[0].name).toBe("Monthly");
+        expect(body.salary_type[1].id).toBe(2);
+        expect(body.salary_type[1].name).toBe("Annually");
+        expect(body.salary_type[2].id).toBe(3);
+        expect(body.salary_type[2].name).toBe("Hourly");
+        expect(body.salary_type[3].id).toBe(4);
+        expect(body.salary_type[3].name).toBe("Weekly");
+        expect(body.salary_type[4].id).toBe(5);
+        expect(body.salary_type[4].name).toBe("One Time");
+        expect(body.salary_type[5].id).toBe(6);
+        expect(body.salary_type[5].name).toBe("Bi-Weekly");
+        expect(body.salary_type[6].id).toBe(7);
+        expect(body.salary_type[6].name).toBe("Others");
+        expect(body.experience_level[0].id).toBe(1);
+        expect(body.experience_level[0].name).toBe("Junior");
+        expect(body.experience_level[1].id).toBe(2);
+        expect(body.experience_level[1].name).toBe("Intermediate");
+        expect(body.experience_level[2].id).toBe(3);
+        expect(body.experience_level[2].name).toBe("Senior");
+        expect(body.job_type[0].id).toBe("on-site");
+        expect(body.job_type[0].name).toBe("On-site");
+        expect(body.job_type[1].id).toBe("remote");
+        expect(body.job_type[1].name).toBe("Remote");
+        expect(body.job_type[2].id).toBe("hybrid");
+        expect(body.job_type[2].name).toBe("Hybrid");
+    });
+
+    test("GET without valid credentials @happy", async ({ request }) => {
+        const response = await request.get(`/api/v2/job/information-meta-data`, {
+            headers: {
+                "ACCEPT": "application/json",
+            }
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body.employment_type[0].id).toBe(1);
+        expect(body.employment_type[0].name).toBe("Permanent");
+        expect(body.employment_type[1].id).toBe(2);
+        expect(body.employment_type[1].name).toBe("Contractual");
+        expect(body.employment_type[2].id).toBe(3);
+        expect(body.employment_type[2].name).toBe("Full Time");
+        expect(body.employment_type[3].id).toBe(4);
+        expect(body.employment_type[3].name).toBe("Part Time");
+        expect(body.employment_type[4].id).toBe(5);
+        expect(body.employment_type[4].name).toBe("Freelance");
+        expect(body.employment_type[5].id).toBe(6);
+        expect(body.employment_type[5].name).toBe("Internship");
+        expect(body.employment_type[6].id).toBe(7);
+        expect(body.employment_type[6].name).toBe("College Program");
+        expect(body.employment_type[7].id).toBe(99);
+        expect(body.employment_type[7].name).toBe("Other");
+        expect(body.salary_type[0].id).toBe(1);
+        expect(body.salary_type[0].name).toBe("Monthly");
+        expect(body.salary_type[1].id).toBe(2);
+        expect(body.salary_type[1].name).toBe("Annually");
+        expect(body.salary_type[2].id).toBe(3);
+        expect(body.salary_type[2].name).toBe("Hourly");
+        expect(body.salary_type[3].id).toBe(4);
+        expect(body.salary_type[3].name).toBe("Weekly");
+        expect(body.salary_type[4].id).toBe(5);
+        expect(body.salary_type[4].name).toBe("One Time");
+        expect(body.salary_type[5].id).toBe(6);
+        expect(body.salary_type[5].name).toBe("Bi-Weekly");
+        expect(body.salary_type[6].id).toBe(7);
+        expect(body.salary_type[6].name).toBe("Others");
+        expect(body.experience_level[0].id).toBe(1);
+        expect(body.experience_level[0].name).toBe("Junior");
+        expect(body.experience_level[1].id).toBe(2);
+        expect(body.experience_level[1].name).toBe("Intermediate");
+        expect(body.experience_level[2].id).toBe(3);
+        expect(body.experience_level[2].name).toBe("Senior");
+        expect(body.job_type[0].id).toBe("on-site");
+        expect(body.job_type[0].name).toBe("On-site");
+        expect(body.job_type[1].id).toBe("remote");
+        expect(body.job_type[1].name).toBe("Remote");
+        expect(body.job_type[2].id).toBe("hybrid");
+        expect(body.job_type[2].name).toBe("Hybrid");
+    });
+});
