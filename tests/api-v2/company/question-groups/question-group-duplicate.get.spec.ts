229e58c94b4876bde8785edbea4491ac9b14dc2d
Abu Hurayra <hurayraiit@gmail.com>
2025-02-03 16:01:27 +0600

    added test for question group duplicate

--- tests/api-v2/company/question-groups/question-group-duplicate.get.spec.ts
+++ tests/api-v2/company/question-groups/question-group-duplicate.get.spec.ts
@@ -13,12 +13,63 @@ test.describe("/api/v2/company/question/group/{group}/duplicate GET requests @co
     test("GET duplicate with valid credentials @happy", async ({ request }) => {
         // Company one should be able to duplicate his own question set
         const set = await createQuestionSet(authObjects.companyOneAuthHeaders);
-        const question_set = await duplicateQuestionSet(authObjects.companyOneAuthHeaders, set.id);
+        const response = await request.get(`/api/v2/company/question/group/${set.id}/duplicate`, {
+            headers: authObjects.companyOneAuthHeaders
+        });
 
-        expect(question_set.id).toBeGreaterThan(set.id);
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("SUCCESS");
+        expect(body.data.name).toBe(set.name);
+        expect(body.data.exam_type).toBe(set.exam_type.id);
+        expect(body.data.company_id).toBe(set.company_id);
+        expect(body.data.exam_duration).toBeNull();
+        expect(body.data.marks_per_question).toBeNull();
+        expect(body.data.created_by).toBeTruthy();
+        expect(body.data.updated_by).toBeTruthy();
+        expect(body.data.deleted_at).toBeNull();
+        expect(body.data.internal_note).toBeTruthy();
+        expect(body.data.note).toBeTruthy();
+        expect(body.data.created_at).toBeTruthy();
+        expect(body.data.updated_at).toBeTruthy();
+        expect(body.data.id).toBeGreaterThan(set.id);
+        expect(body.message).toBe("Question set duplicated.");
     });
 
-    test("GET duplicate without auth token", async ({ request }) => {
+    test("GET duplicate with other companies credentials @security", async ({ request }) => {
+        // Company two should not be able to duplicate company one's question set
+        const set = await createQuestionSet(authObjects.companyOneAuthHeaders);
+        const response = await request.get(`/api/v2/company/question/group/${set.id}/duplicate`, {
+            headers: authObjects.companyTwoAuthHeaders
+        });
+
+        expect(response.status()).toBe(480);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("FAILED");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("You do not have sufficient permissions.");
+    });
+
+    test("GET duplicate with candidates credentials @security", async ({ request }) => {
+        // Candidates should not be able to duplicate question set
+        const set = await createQuestionSet(authObjects.companyOneAuthHeaders);
+        const response = await request.get(`/api/v2/company/question/group/${set.id}/duplicate`, {
+            headers: authObjects.candidateOneAuthHeaders
+        });
+        expect.soft(response.status()).toBe(480);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("failed");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("You do not have access permissions.");
+    });
+
+    test("GET duplicate without auth token @security", async ({ request }) => {
         const set = await createQuestionSet(authObjects.companyOneAuthHeaders);
         const response = await request.get(`/api/v2/company/question/group/${set.id}/duplicate`, {
             headers: {
