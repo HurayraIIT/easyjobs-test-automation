67e579e5de58c02dc5bef72d3ea740c13bb516af
Abu Hurayra <hurayraiit@gmail.com>
2025-02-12 18:40:02 +0600

    added more tests on company settings endpoints

---
+++ tests/api-v2/company/settings/application-setting.get.spec.ts
@@ -0,0 +1,72 @@
+//GET: /api/v2/company/setting/application-setting
+
+import { test, expect } from '@playwright/test';
+import authObjects from '@datafactory/auth';
+import { createAssertions } from "@helpers/createAssertions";
+
+test.describe("/api/v2/company/setting/application-setting GET requests @company", async () => {
+    test("GET with valid credentials @happy", async ({ request }) => {
+        const response = await request.get(`/api/v2/company/setting/application-setting`, {
+            headers: authObjects.companyOneAuthHeaders
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("SUCCESS");
+        expect(body.data.login_option).toEqual(["email", "google", "linkedin"]);
+        expect(body.data.all_options.google).toBe("Google");
+        expect(body.data.all_options.linkedin).toBe("LinkedIn");
+        expect(body.data.all_options.email).toBe("Email");
+        expect(body.data.attach_resume_with_email).toBe(true);
+        expect(body.message).toBeNull();
+    });
+
+    test("GET with invalid credentials", async ({ request }) => {
+        const response = await request.get(`/api/v2/company/setting/application-setting`, {
+            headers: {
+                "Accept": "application/json",
+            }
+        });
+
+        expect(response.status()).toBe(401);
+
+        const body = await response.json();
+        expect(body.message).toBe("Unauthenticated.");
+    });
+
+    test("GET with valid credentials but another company ID", async ({ request }) => {
+        // Company two should not be able to access data from company one
+        const maliciousHeaders = authObjects.companyTwoAuthHeaders;
+        maliciousHeaders['Company-Id'] = authObjects.companyOneAuthHeaders['Company-Id'];
+        maliciousHeaders['State-Version'] = authObjects.companyOneAuthHeaders['State-Version'];
+
+        const response = await request.get(`/api/v2/company/setting/application-setting`, {
+            headers: maliciousHeaders
+        });
+
+        expect(response.status()).toBe(400);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("FAILED");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("Something went wrong.");
+    });
+
+    test("GET with candidate auth", async ({ request }) => {
+        const response = await request.get(`/api/v2/company/setting/application-setting`, {
+            headers: authObjects.candidateOneAuthHeaders
+        });
+
+        expect(response.status()).toBe(480);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body.status).toBe("failed");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("You do not have access permissions.");
+    });
+});
---
+++ tests/api-v2/company/settings/apply-setting-update.post.spec.ts
@@ -0,0 +1,80 @@
+//POST: /api/v2/company/setting/apply-setting/update
+
+import { test, expect } from '@playwright/test';
+import authObjects from '@datafactory/auth';
+import { createAssertions } from "@helpers/createAssertions";
+
+const data = {
+    "id": "attach_resume_with_email",
+    "title": "Attach resume with email",
+    "e_icon": "e-document",
+    "hint": "Enable this to get a copy of the applicant’s resume through email notification. This helps you see the resume without logging in to easy.jobs.",
+    "selected": true
+};
+
+test.describe("/api/v2/company/setting/apply-setting/update POST requests @company", async () => {
+    test("POST with valid credentials @happy", async ({ request }) => {
+        const response = await request.post(`/api/v2/company/setting/apply-setting/update`, {
+            headers: authObjects.companyOneAuthHeaders,
+            data: data
+        });
+
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("SUCCESS");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("Apply setting updated.");
+    });
+
+    test("POST with invalid credentials", async ({ request }) => {
+        const response = await request.post(`/api/v2/company/setting/apply-setting/update`, {
+            headers: {
+                "Accept": "application/json"
+            },
+            data: data
+        });
+
+        expect(response.status()).toBe(401);
+
+        const body = await response.json();
+        expect(body.message).toBe("Unauthenticated.");
+    });
+
+    test("POST with valid credentials but another company ID", async ({ request }) => {
+        // Company two should not be able to modify data from company one
+        const maliciousHeaders = authObjects.companyTwoAuthHeaders;
+        maliciousHeaders['Company-Id'] = authObjects.companyOneAuthHeaders['Company-Id'];
+        maliciousHeaders['State-Version'] = authObjects.companyOneAuthHeaders['State-Version'];
+
+        const response = await request.post(`/api/v2/company/setting/apply-setting/update`, {
+            headers: maliciousHeaders,
+            data: data
+        });
+
+        expect(response.status()).toBe(471);
+
+        const body = await response.json();
+        // await createAssertions(body);
+        expect(body.status).toBe("FAILED");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("Something went wrong.");
+    });
+
+    test("POST with candidate auth", async ({ request }) => {
+        const response = await request.post(`/api/v2/company/setting/apply-setting/update`, {
+            headers: authObjects.candidateOneAuthHeaders,
+            data: data
+        });
+
+        expect(response.status()).toBe(480);
+
+        const body = await response.json();
+
+        // await createAssertions(body);
+        expect(body.status).toBe("failed");
+        expect(body.data).toEqual([]);
+        expect(body.message).toBe("You do not have access permissions.");
+    });
+});
